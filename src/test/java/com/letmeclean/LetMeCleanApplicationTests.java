dec3279638fb028845b5fd26d2152b1b84e73739
23Yong <66981851+23Yong@users.noreply.github.com>
2022-07-01 23:16:19 +0900

    #1 프로젝트 생성

---
+++ src/test/java/com/letmeclean/LetMeCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letmeclean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetMeCleanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
