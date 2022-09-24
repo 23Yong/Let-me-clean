3dce9dc23eb5cd342a562d75f3d31705f54a4ec3
23Yong <1239824@naver.com>
2022-09-24 22:55:36 +0900

    #50 - 수거요청 URL에 대한 시큐리티 파일 수정
    - 수거요청에서 발생할 수 있는 에러코드 추가
    - Orders엔티티에 대한 레포지토리 추가
    - Orders의 상태를 저장하기 위한 OrdersState 추가

--- src/main/java/com/letmeclean/global/exception/ErrorCode.java
+++ src/main/java/com/letmeclean/global/exception/ErrorCode.java
@@ -16,13 +16,13 @@ public enum ErrorCode {
     BAD_REQUEST_PAYMENT_READY(BAD_REQUEST, "이미 진행중인 결제가 존재합니다."),
     BAD_REQUEST_PAYMENT_APPROVE(BAD_REQUEST, "처리하고자 하는 결제 정보가 존재하지 않습니다."),
     BAD_REQUEST_PREV_PASSWORD(BAD_REQUEST, "이전 비밀번호와 일치하지 않습니다."),
-    BAD_REQUEST_COUPON_EXPIRED_TIME(BAD_REQUEST, "쿠폰의 유효기간이 생성시간보다 빠릅니다."),
+    ALREADY_USED_TICKET(BAD_REQUEST, "이미 사용한 티켓입니다."),
 
     /* 404 NOT_FOUND */
     MEMBER_NOT_FOUND(NOT_FOUND, "해당 사용자 정보를 찾을 수 없습니다."),
     TICKET_NOT_FOUND(NOT_FOUND, "해당 티켓 정보를 찾을 수 없습니다."),
     COUPON_NOT_FOUND(NOT_FOUND, "해당 쿠폰 정보를 찾을 수 없습니다."),
-    ISSUED_COUPON_NOT_FOUND(NOT_FOUND, "해당 발급 쿠폰 정보를 찾을 수 없습니다."),
+    ISSUED_TICKET_NOT_FOUND(NOT_FOUND, "해당 발급 티켓 정보를 찾을 수 없습니다."),
 
     /* 409 CONFLICT */
     DUPLICATE_EMAIL_CONFLICT(CONFLICT, "이미 해당 이메일 정보가 존재합니다."),
